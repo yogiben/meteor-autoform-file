b3c104fb1f8ccaa0c3e66268bcfc54e9b65e19db
Ben Jones <benjaminpeterjonesesquire@gmail.com>
2014-10-28 13:07:21 +0000

    bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Upload and manage files easily with AutoForm and CollectionFS",
-  version: "0.0.1",
+  version: "0.0.2",
   git: "http://github.com/yogiben/autoform-file.git"
 });
 
