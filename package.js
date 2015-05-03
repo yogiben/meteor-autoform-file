b0843dc52fb15d5717f8397b4b648b55a6472e62
Ben Jones <benjaminpeterjonesesquire@gmail.com>
2015-05-03 20:52:56 +0100

    add_files to addFiles

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: "yogiben:autoform-file",
   summary: "File upload for AutoForm",
   description: "File upload for AutoForm",
-  version: "0.2.4",
+  version: "0.2.5",
   git: "http://github.com/yogiben/autoform-file.git"
 });
 
@@ -19,7 +19,7 @@ Package.onUse(function(api) {
     ],
     'client');
 
-  api.add_files('lib/client/autoform-file.html', 'client');
-  api.add_files('lib/client/autoform-file.less', 'client');
-  api.add_files('lib/client/autoform-file.coffee', 'client');
+  api.addFiles('lib/client/autoform-file.html', 'client');
+  api.addFiles('lib/client/autoform-file.less', 'client');
+  api.addFiles('lib/client/autoform-file.coffee', 'client');
 });
