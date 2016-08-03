ad14d5ad47a5f6a20d998546e512e8aaf0b0d834
Dmitriy A. Golev <ceo@veliov.com>
2016-08-03 23:28:31 +0300

    v1.0.0

--- package.js
+++ package.js
@@ -2,25 +2,24 @@ Package.describe({
   name: "ostrio:autoform-files",
   summary: "File upload for AutoForm using ostrio:files",
   description: "File upload for AutoForm using ostrio:files",
-  version: "0.1.0",
+  version: "1.0.0",
   git: "https://github.com/VeliovGroup/meteor-autoform-file.git"
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@1.3');
+  api.versionsFrom('METEOR@1.4');
 
   api.use([
     'check',
     'underscore',
     'reactive-var',
     'templating',
-    'less@1.0.0 || 2.5.1',
     'aldeed:autoform@5.8.0',
-    'ostrio:files@1.5.6'
+    'ostrio:files@1.6.9'
   ]);
 
   api.addFiles('lib/client/autoform.js', 'client');
   api.addFiles('lib/client/fileUpload.html', 'client');
   api.addFiles('lib/client/fileUpload.js', 'client');
-api.addFiles('lib/client/uploadImageDemo.html', 'client');
+  api.addFiles('lib/client/uploadImageDemo.html', 'client');
 });
