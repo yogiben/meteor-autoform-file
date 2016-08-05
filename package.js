f330f9961212c59f8bba6b141840b052ea67b057
Dmitriy A. Golev <ceo@veliov.com>
2016-08-05 20:52:34 +0300

    v1.0.2
     - Better validation errors (*finally hacked*)

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: "ostrio:autoform-files",
   summary: "File upload for AutoForm using ostrio:files",
   description: "File upload for AutoForm using ostrio:files",
-  version: "1.0.1",
+  version: "1.0.2",
   git: "https://github.com/VeliovGroup/meteor-autoform-file.git"
 });
 
