8d7d7858c07721453e7725a231e079510fa40e7e
jankapunkt <jkuester@uni-bremen.de>
2017-07-24 10:50:58 +0200

    use Mongo.Collection.get to get collection instance by name

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'ostrio:autoform-files',
   summary: 'File upload for AutoForm using ostrio:files',
   description: 'File upload for AutoForm using ostrio:files',
-  version: '2.0.1',
+  version: '2.0.2',
   git: 'https://github.com/VeliovGroup/meteor-autoform-file.git'
 });
 
@@ -13,6 +13,7 @@ Package.onUse(function(api) {
     'check',
     'ecmascript',
     'underscore',
+    'mongo',
     'reactive-var',
     'templating',
     'aldeed:autoform@6.2.0',
